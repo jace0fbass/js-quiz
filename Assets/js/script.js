0ad08f8dc28156a77a980a2f8c6ba87b4bd31db3
Jason <jajones414@gmail.com>
2022-09-20 20:42:49 -0500

    I have added a header, h1, timebox section, footer, and notes to the index.html file.

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// use prompts that take in user input that has to be correct to proceed.
+// make a timer.
+// have timer start on click.
+// display answer result alerts.
+// display final score
+// take in user initials.
+// cache scores .
+// log and rotate 5 highest scores as they change. (how tf?)
+// display high score rankings.
