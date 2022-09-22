bf44e08c278e24aec91b71c97c30c28884ffe75b
Jason <jajones414@gmail.com>
2022-09-21 23:37:36 -0500

    I have removed most items in the style.css file. I have added list items to the script.js file. I have cleaned up the index.html file.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,19 +2,6 @@
 var ol = document.querySelector("ol")
 var button = document.querySelector("button")
 
-$('<ol>').text("Commonly used data types DO NOT include:").appendTo('body')
-$('<li1>').text("1. Strings ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Booleans ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. Alerts ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. Numbers ").appendTo('ol').appendTo('body')
-    console.log("first question")
-// questions selector
-//ol.addEventListener("click", function(event) {
-    //if (event.target.matches("li")) {
-        //console.log(event.target.innerText)
-    //}
-//})
-//button to start countdown
 $('<button>').text("Start Quiz").appendTo('body')
 //button.addEventListener("click", function(event) {
     //if (event.target.matches("")) {
@@ -22,6 +9,51 @@ $('<button>').text("Start Quiz").appendTo('body')
   //}
 //})
 
+// need to create function to run through these prompts but stop them if they get the answer wrong answer. finish after 5 questions or 1 minute. 
+// need if statements
+
+$('<ol>').text("Commonly used data types DO NOT include:").appendTo('body')
+$('<li1>').text("1. Strings ").appendTo('ol').appendTo('body') 
+$('<li2>').text("2. Booleans ").appendTo('ol').appendTo('body') 
+$('<li3>').text("3. Alerts ").appendTo('ol').appendTo('body') 
+$('<li4>').text("4. Numbers ").appendTo('ol').appendTo('body')
+console.log("first question")
+
+$('<ol>').text("The condition in an if else statement is enclosed within _____.").appendTo('body')
+$('<li1>').text("1. Quotes ").appendTo('ol').appendTo('body') 
+$('<li2>').text("2. Curly Brackets ").appendTo('ol').appendTo('body') 
+$('<li3>').text("3. Parentheses ").appendTo('ol').appendTo('body') 
+$('<li4>').text("4. Square Brackets ").appendTo('ol').appendTo('body')
+    console.log("second question")
+
+$('<ol>').text("Arrays in Javascript can be used to store _____.").appendTo('body')
+$('<li1>').text("1. Numbers and Strings ").appendTo('ol').appendTo('body') 
+$('<li2>').text("2. Other Arrays ").appendTo('ol').appendTo('body') 
+$('<li3>').text("3. Booleans ").appendTo('ol').appendTo('body') 
+$('<li4>').text("4. All of the above ").appendTo('ol').appendTo('body')
+    console.log("third question")
+
+$('<ol>').text("String values must be enclosed within _____ when being assigned to variables.").appendTo('body')
+$('<li1>').text("1. Commas ").appendTo('ol').appendTo('body') 
+$('<li2>').text("2. Curly Brackets ").appendTo('ol').appendTo('body') 
+$('<li3>').text("3. Quotes ").appendTo('ol').appendTo('body') 
+$('<li4>').text("4. Parenthesis ").appendTo('ol').appendTo('body')
+    console.log("fourth question")
+
+$('<ol>').text("A very useful tool used during development and debugging for printing content to the debugger is:").appendTo('body')
+$('<li1>').text("1. Javascript ").appendTo('ol').appendTo('body') 
+$('<li2>').text("2. Terminal/Bash ").appendTo('ol').appendTo('body') 
+$('<li3>').text("3. for loops ").appendTo('ol').appendTo('body') 
+$('<li4>').text("4. console log ").appendTo('ol').appendTo('body')
+    console.log("fifth question")
+
+// questions selector
+//ol.addEventListener("click", function(event) {
+    //if (event.target.matches("li")) {
+        //console.log(event.target.innerText)
+    //}
+//})
+//button to start countdown
 
 //child of child for list items
 
