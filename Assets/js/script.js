1c1cf2915f396f1160c910186764ba79370a501f
Jason <jajones414@gmail.com>
2022-09-24 12:09:34 -0500

    I have edited the index.html and script.js files to be easier to read.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,12 +6,13 @@
 // cache scores .
 // log and rotate 5 highest scores as they change. (how tf?)
 // display high score rankings.
-var startBtn = document.querySelector("#startBtn")
 var userInput = document.querySelector("#choices")
-var index = 0
+var startBtn = document.querySelector("#startBtn")
+var startBtn = document.getElementById("#startBtn")
 var startPage = document.querySelector("#startPage")
-
-// 
+var index = 0
+var score = 0
+var container = ""
 var quizQuestions = [{
     question:  "Commonly used data types DO NOT include:",
     correct: 'Alerts', 
@@ -38,29 +39,34 @@ var quizQuestions = [{
     choices: ['Quotes', 'Booleans', 'Alerts', 'Numbers']
 }
 ]
+
 function startQuiz() {
     console.log("im clicked");
     startPage.setAttribute("class", "hide")
-}
-// function to run through the quiz questions
-// function quizRender() {
-    //     var currentQuestion = quizQuestions[index];
-    //     question.innerText = currentQuestion.question;
-    //    userInput.innerHTML ="";
     
-    //      for (var i = 0; i < currentQuestion.choices.length; i++) {
-//         var btnChoice = document.createElement('button');
-//        var choice = currentQuestion.choices[index];
-//       //  btnChoice.innerText = choice;
-//         btnChoice.setAttribute("class", "choice")
-//         btnChoice.setAttribute("value", choice)
-//         btnChoice.onclick = checkClick;
-//        userInput.appendChild(btnChoice);
-//        console.log('loop');
-//     }
-// }
+    // function to run through the quiz questions
+    function quizRender() {
+        console.log(rendered)
+        var currentQuestion = quizQuestions[index];
+        question.innerText = currentQuestion.question;
+        userInput.innerHTML ="";
+        
+        for (var i = 0; i < currentQuestion.choices.length; i++) {
+            var btnChoice = document.createElement('button');
+            var choice = currentQuestion.choices[index];
+            btnChoice.setAttribute("class", "choice")
+            btnChoice.setAttribute("value", choice)
+            btnChoice.onclick = checkClick;
+            userInput.appendChild(btnChoice);
+            if (btnChoice === quizQuestions('correct')) {
+                quizQuestions++
+            }
+            console.log('loop');
+        }
+    }
+    
+    startBtn.addEventListener("click", startQuiz());
+}
 
-
-//button to start countdown
-
-startBtn.addEventListener("click", startQuiz());
+    //button to start countdown
+    
