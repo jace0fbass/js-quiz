b4292a533422ee9d8d6f8721bc9e4159c0424a33
Jason <jajones414@gmail.com>
2022-09-23 21:27:28 -0500

    I have removed the jQuery button for now and added a placeholder button in the index.html and I have also added a quizRender function that doesnt work yet.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,94 +1,66 @@
-var ol = document.querySelector("ol")
+var jH1 = document.querySelector("jH1")
 var button = document.querySelector("button")
-var resultAlert = document.querySelector(alert("Correct!"))
+var userInput = document.querySelector("choices")
+var correct = document.querySelector(quizQuestions === true)
+var index = 0
+
 
 var quizQuestions = [{
     question:  "Commonly used data types DO NOT include:",
-    correct: resultAlert, 
+    correct: 'Alerts', 
     choices: ['Strings', 'Booleans', 'Alerts', 'Numbers']
 },
 {
     question:  "The condition in an if else statement is enclosed within _____.",
-    correct: resultAlert, 
+    correct: 'Curly Brackets', 
     choices: ['Quotes', 'Curly Brackets', 'Parentheses', 'Square Brackets']
 },
 {
     question:  "Arrays in Javascript can be used to store _____.",
-    correct: resultAlert, 
+    correct: 'All of the above', 
     choices: ['Numbers and Arrays', 'Booleans', 'Other Arrays', 'All of the above']
 },
 {
     question:  "String values must be enclosed within _____ when being assigned to variables.",
-    correct: resultAlert, 
+    correct: 'Commas', 
     choices: ['Commas', 'Curly Brackets', 'Quotes', 'Parentheses']
 },
 {
     question:  "A very useful tool used during development and debugging for printing content to the debugger is:",
-    correct: resultAlert, 
-    choices: ['Quotes', 'booleans', 'alerts', 'numbers']
+    correct: 'Booleans', 
+    choices: ['Quotes', 'Booleans', 'Alerts', 'Numbers']
+}
+]
+// function to run through the quiz questions
+function quizRender() {
+    var currentQuestion = quizQuestions[i]
+    jH1.innerText = currentQuestion.question
+    userInput.innerHTML ="" //clear loop
+
+    for (i = 0; i < currentQuestion.choices.length; i++) {
+        var li = document.createElement('li')
+        var choice = currentQuestion.choices[i]
+        li.innerText = choice
+        userInput.appendChild(li)
+        console.log('loop')
+    }
 }
 
-//]
-//for (i =0; i < quizQuestions.length; i++) {
-    
-//}
-
-$('<button>').text("Start Quiz").appendTo('body')
-//button.addEventListener("click", function(event) {
-    //if (event.target.matches("")) {
-        console.log("button")
-        //}
-        //})
+button.addEventListener("click", function(t) {
+        i++
+        quizRender()
+                })
+     console.log("button")
+        
         
 // use prompts that take in user input that has to be correct to proceed.
 // need to create function to run through these prompts but stop them if they get the answer wrong answer. finish after 5 questions or 1 minute.
 // need if statements
 
 
-/*$('<ul>').text("Commonly used data types DO NOT include:").appendTo('body')
-$('<li1>').text("1. Strings ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Booleans ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. Alerts ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. Numbers ").appendTo('ol').appendTo('body')
-console.log("first question")
 
-$('<ul>').text("The condition in an if else statement is enclosed within _____.").appendTo('body')
-$('<li1>').text("1. Quotes ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Curly Brackets ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. Parentheses ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. Square Brackets ").appendTo('ol').appendTo('body')
-    console.log("second question")
 
-$('<ul>').text("Arrays in Javascript can be used to store _____.").appendTo('body')
-$('<li1>').text("1. Numbers and Strings ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Other Arrays ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. Booleans ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. All of the above ").appendTo('ol').appendTo('body')
-    console.log("third question")
-
-$('<ul>').text("String values must be enclosed within _____ when being assigned to variables.").appendTo('body')
-$('<li1>').text("1. Commas ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Curly Brackets ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. Quotes ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. Parenthesis ").appendTo('ol').appendTo('body')
-    console.log("fourth question")
-
-$('<ul>').text("A very useful tool used during development and debugging for printing content to the debugger is:").appendTo('body')
-$('<li1>').text("1. Javascript ").appendTo('ol').appendTo('body') 
-$('<li2>').text("2. Terminal/Bash ").appendTo('ol').appendTo('body') 
-$('<li3>').text("3. for loops ").appendTo('ol').appendTo('body') 
-$('<li4>').text("4. console log ").appendTo('ol').appendTo('body')
-    console.log("fifth question")
-
-// questions selector
-//ol.addEventListener("click", function(event) {
-    //if (event.target.matches("li")) {
-        //console.log(event.target.innerText)
-    //}
-//})
 //button to start countdown
-
-//child of child for list items
 
 // make a timer.
 // have timer start on click.
